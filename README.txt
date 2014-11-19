4e4cd28e89244ff2414ad2d85d43e841165576c9
Angie Wang <angiewang123@gmail.com>
2014-11-18 21:30:05 -0500

    updated format of readme

---
+++ README.txt
@@ -0,0 +1,8 @@
+Angie Wang's term project for 15-112 Fall 2014
+
+CodeCardio is a game that uses face movements to modify the location of the main character. 
+
+To run: 
+
+Enter the directory: /termproject/codecardio
+Run: python main.py haarcascade_frontalface_default.xml 
---
+++ codecardio/codecardio.py
@@ -0,0 +1,22 @@
+from Tkinter import *
+#from webcam import *
+
+class Character(object):
+	def __init__(x, y):
+		self.x = x
+		self.y = y
+
+	#when collision occurs, generate random question 
+	def checkCollision(): pass
+
+class Player(object):
+	def checkCollision():
+		super(self, Player)
+
+	#generate random question for 112
+	def generateQuestion(): pass
+
+root = Tk()
+root.wm_title("Code cardio")
+root.config(background = "#FFFFFF")
+root.mainloop()
